37b7a52b87baba813eb5361781a88731cb53c3f4
Joe <mythbuster188@gmail.com>
2022-03-20 17:07:51 -0500

    add this.event

--- scriptHelper.js
+++ scriptHelper.js
@@ -52,13 +52,13 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     document.getElementById("fuelStatus").innerText = "There is not enough fuel for the journey."
     document.getElementById("launchStatus").innerText = "Shuttle NOT ready for launch."
     document.getElementById("launchStatus").style.color = "red"
-    event.preventDefault()
+    this.event.preventDefault()
 } else if (Number(cargoLevel) > 10000){
     document.getElementById("faultyItems").style.visibility = "visible"
     document.getElementById("cargoStatus").innerText = "There is too much mass for the shuttle to take off."
     document.getElementById("launchStatus").innerText = "Shuttle NOT ready for launch."
     document.getElementById("launchStatus").style.color = "red"
-    event.preventDefault()
+    this.event.preventDefault()
 } else {
     document.getElementById("launchStatus").innerText = "Shuttle is ready for launch."
     document.getElementById("launchStatus").style.color = "green"
